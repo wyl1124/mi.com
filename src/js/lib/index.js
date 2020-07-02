f185cdd9e3f78e377ecd2aacef693feb19c1c7d5
Wang yulong <254125389@qq.com>
2020-07-02 16:57:00 +0800

    v1.0.6

--- src/js/lib/index.js
+++ src/js/lib/index.js
@@ -7,8 +7,24 @@ define(['jquery'],function($){
                 type: "get",
                 url:`${baseUrl}/src/interface/getall.php`,
                 dataType: "json",
-                success: function (response) {
-                    console.log(response);
+                success: function (res) {
+                    console.log(res);
+                    let temp = '';
+                    res.forEach(elm=>(
+                        temp +=`<div class="i-size-box">
+                                <img class="img-agent lazyload" src="../img/2.jpg">
+                            </div>
+                            <p class="i-size-tit is-tags-mark"><u class="jsd-tag"></u>MM 肥牛火锅片 900g</p>
+                            <span data-pmid="77944676" data-productid="66384732"><em>¥</em>73.8</span>
+                            <div class="marking save">
+                                <div class="boxs">
+                                    <i>已省</i>
+                                    <p><em>¥</em>12.6</p>
+                                </div>
+                            </div>`
+                    ))
+
+                    $('item').html(temp);
                 }
             });
         }
